7560fa650c74fa1c9d43faa8e2aa9eb880bac0a0
Alexander Khryukin <alexander@mezon.ru>
2015-11-30 16:37:33 +0300

    refactoring, and added part to check *.so files

--- app_checker.sh
+++ app_checker.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# The section in a .desktop file we need.
-SECTION="Desktop Entry"
-
 # The directory with the results
 RESULT_DIR="/tmp/results"
 PACKAGE_QL="/tmp/rpmql_$PKG"
@@ -33,6 +30,33 @@ echo "Processing $PKG"
 if [ ! -d "$RESULT_DIR" ]; then
   mkdir -p $RESULT_DIR
 fi
+
+run_executable(){
+echo -e "searching for a binary file"
+binary_file=$(cat $PACKAGE_QL | grep "/usr/bin/\|/bin/\|/sbin/\|/usr/sbin/")
+if [ $? -eq 1 ]
+then
+echo "Package $PKG not contain files in bindirs (/usr/bin/,/bin/,/sbin, etc)"
+else
+echo -e "run ldd on $PKG executable binaries" >> $RESULT_DIR/app_test_$PKG
+$LDD_RUN $binary_file | grep "$PATTERN" >> $RESULT_DIR/ld_log_$PKG
+$FILE_RUN $binary_file >> $RESULT_DIR/app_test_$PKG
+fi
+}
+
+run_libraries(){
+echo -e "searching package libs"
+library_file=$(cat $PACKAGE_QL | grep ".so")
+if [ $? -eq 1 ]
+then
+echo "Package $PKG not contain libs (*.so)"
+else
+echo -e "run ldd on $PKG libraries files" >> $RESULT_DIR/app_test_$PKG
+$LDD_RUN $library_file | grep "$PATTERN" >> $RESULT_DIR/ld_log_$PKG
+$FILE_RUN $library_file >> $RESULT_DIR/app_test_$PKG
+fi
+}
+
 rpmql_package=$(rpm -q -l $PKG > $PACKAGE_QL)
 result=$?
 if [ $result -eq 0 ]; then
@@ -41,21 +65,24 @@ if [ $result -eq 0 ]; then
         result=$?
         if [ $result -eq 0 ]; then
                 command_to_run=$(cat $desktop_file | grep '^Exec'| sed 's/^Exec=//')
-                echo "$command_to_run extracted from $desktop_file"
-		echo "test me from python script"
-#		xvfb-run $command_to_run & sleep $DEFAULT_TIMEOUT && kill -9 $!
-        else
-                binary_file=$(cat $PACKAGE_QL | grep "/usr/bin/\|/bin/\|/sbin/\|/usr/sbin/")
-		$LDD_RUN $binary_file | grep "$PATTERN" >> $RESULT_DIR/ld_log_$PKG
+		run_libraries
 		if [ "$(grep 'not found' $RESULT_DIR/ld_log_$PKG)" ]; then
 			echo "not ok"
 			echo "Package does not working properly, check libs"
         		echo "$PKG" >> $RES_FAILED_TO_CHECK
 		else
-			echo -e "run ldd on $PKG" >> $RESULT_DIR/app_test_$PKG
-			$LDD_RUN $binary_file >> $RESULT_DIR/app_test_$PKG
-			echo -e "run file on $PKG" >> $RESULT_DIR/app_test_$PKG
-			$FILE_RUN $binary_file >> $RESULT_DIR/app_test_$PKG
+			echo "ldd says that's $PKG is good"
+		fi
+                echo "$command_to_run extracted from $desktop_file"
+		echo "test executable from python script"
+        else
+		run_executable
+		run_libraries
+		if [ "$(grep 'not found' $RESULT_DIR/ld_log_$PKG)" ]; then
+			echo "not ok"
+			echo "Package does not working properly, check libs"
+        		echo "$PKG" >> $RES_FAILED_TO_CHECK
+		else
 			echo "ldd says that's $PKG is good"
 		fi
 		
